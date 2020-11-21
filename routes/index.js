893848acec454290a5041fe7d5f4cfcf4033feca
Valentin <v.backx@gmail.com>
2020-11-21 20:14:09 +0100

    Offreurs
    multiples offreurs avec noms

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 
 var router = express.Router();
-const CONFIG = require('../config.json')
 
+const BOARD_LISTS_URL = "/1/boards/"+process.env.BOARD_LISTS_ID+"/lists"
 
-const BOARD_LISTS_URL = "/1/boards/"+CONFIG.BOARD_LISTS_ID+"/lists"
+const fetch = require('node-fetch');
 
 const Trello = require('node-trello')
-const t = new Trello(CONFIG.TRELLO_API_ID, CONFIG.TRELLO_SERVER_TOKEN);
+const t = new Trello(process.env.TRELLO_API_ID, process.env.TRELLO_SERVER_TOKEN);
+var AllLists ;
+
 
 function getAllCards(lists,listIndex,callback,res)
 {
@@ -17,9 +19,9 @@ function getAllCards(lists,listIndex,callback,res)
     return;
   }
   t.get("1/lists/"+lists[listIndex].id+"/cards",function(err,cards){
-    if(lists[listIndex].name.includes(CONFIG.PICKED_LIST_NAME))
+    if(lists[listIndex].name.includes("[OFFREUR]"))
     {
-      res.doneList = cards
+      res.doneLists.push({cards:cards,name:lists[listIndex].name.replace('[OFFREUR]','')})
     }else{
       res.wishLists.push({name:lists[listIndex].name,cards:cards})
     }
@@ -29,11 +31,11 @@ function getAllCards(lists,listIndex,callback,res)
 
 function getLists(response)
 {
-  t.get(BOARD_LISTS_URL,function(err,lists){
-      let res = {wishLists :[],doneList : {}}
+  t.get(BOARD_LISTS_URL,function(err,lists) {
+      AllLists = lists
+      let res = {wishLists :[],doneLists : []}
       if(err) throw err
       getAllCards(lists,0,function(data){response.render('index',data)},res);
-
   })
 }
 
@@ -42,29 +44,30 @@ router.get('/', function(req, res, next) {
   getLists(res)
 });
 
-function getDoneListId(callback)
-{
-  t.get(BOARD_LISTS_URL,function(err,lists){
-    if(err) throw err
-    for(let i =0;i < lists.length;i++)
-    {
-      if(lists[i].name.includes(CONFIG.PICKED_LIST_NAME))
-      {
-        callback(lists[i].id)
-        return
-      }
-    }
-    throw "Done list not found"
-  })
-}
+router.post('/',async function(req,res,next) {
 
-router.post('/',function(req,response,next){
-  console.log(req.body.cardId);
-  getDoneListId(function(doneListId){
-    t.put("/1/cards/"+req.body.cardId+"/idList",{value:doneListId},function(err,res){
-      if(err) throw err
-      response.redirect('/')
+  let properList = AllLists.filter(e => e.name.includes("[OFFREUR]"+req.body.offreur))[0]
+  let offreurName = req.body.offreur
+  let properListId;
+  if(!properList)
+  {
+    //create a list for this offreur
+    let response = await fetch('https://api.trello.com/1/boards/'+process.env.BOARD_LISTS_ID+'/lists?key='+process.env.TRELLO_API_ID+'&token='+process.env.TRELLO_SERVER_TOKEN+'&name=[OFFREUR]'+encodeURI(offreurName), {
+      method: 'POST'
     })
+    let result = await fetch('https://api.trello.com/1/boards/'+process.env.BOARD_LISTS_ID+'/lists?key='+process.env.TRELLO_API_ID+'&token='+process.env.TRELLO_SERVER_TOKEN,{
+        method:'GET'
+    })
+    let json = await result.json()
+    properListId = json.filter(e => e.name=='[OFFREUR]'+offreurName)[0].id
+  }else
+  {
+    properListId = properList.id
+  }
+
+  t.put("/1/cards/"+req.body.cardId+"/idList",{value:properListId},function(err,resHere){
+    if(err) throw err
+    res.redirect('/')
   })
 });
 
